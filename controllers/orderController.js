3352020ef60bfa37647356e31f10410b486c9be6
dipesh kumar khadgi <d8khadgi@gmail.com>
2025-03-02 12:54:58 +0545

    user order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -69,3 +69,43 @@ const listOrders = async (req, res) => {
     }
 }
 
+// User Orders for Frontend
+const userOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({ userId: req.body.userId });
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+const updateStatus = async (req, res) => {
+    console.log(req.body);
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status });
+        res.json({ success: true, message: "Status Updated" })
+    } catch (error) {
+        res.json({ success: false, message: "Error" })
+    }
+
+}
+
+const verifyOrder = async (req, res) => {
+    const {orderId , success} = req.body;
+    try {
+        if (success==="true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" })
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId)
+            res.json({ success: false, message: "Not Paid" })
+        }
+    } catch (error) {
+        res.json({ success: false, message: "Not  Verified" })
+    }
+
+}
+
+export { placeOrder, listOrders, userOrders, updateStatus ,verifyOrder }
