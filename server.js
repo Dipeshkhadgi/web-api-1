60ec1366a0d6f0e238f44f6e5620ee4cfc012f4c
dipesh kumar khadgi <d8khadgi@gmail.com>
2025-03-02 15:58:55 +0545

    server add

---
+++ server.js
@@ -0,0 +1,15 @@
+import express  from "express"
+import cors from 'cors'
+import morgan from "morgan"
+import ConnectDB  from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
+
+
