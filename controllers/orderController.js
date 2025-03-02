0c664f1582b4a899bc6a62aa806b50c097ac370a
dipesh kumar khadgi <d8khadgi@gmail.com>
2025-03-02 12:54:24 +0545

    order admin

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -58,3 +58,14 @@ const placeOrder = async (req, res) => {
     }
 }
 
+// Listing Order for Admin panel
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({ success: true, data: orders })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
+
